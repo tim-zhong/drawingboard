e471990f56162749ab44fc30404783fec6d5ef9c
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-04-03 16:57:25 -0400

    cast to number

--- wss/wss.js
+++ wss/wss.js
@@ -141,8 +141,7 @@ function boardprocess(obj){
 			lastalpha = alpha;
 
 			//adjust alpha
-			alpha = alpha+0;
-			alpha += alpharounds * 360;
+			alpha += Number(alpharounds) * 360 + Number(alpha);
 			console.log('alpharounds: '+alpharounds+'  adjusted alpha: '+alpha);
 
 
