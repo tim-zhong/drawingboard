c05fd2669f02c892cff55e18ee0ad6aad538eb02
Runbiao Zhong <timzhong2374@hotmail.co.uk>
2016-05-15 10:48:24 -0400

    jh

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ canvas {display: block;}
     <h1>Welcome to the Drawingboard!!</h1>
     <h2>Please Follow the instructions below</h2>
     <ul id="instructions">
-      <li>1. Open this page on a PC</li>
+      <li>1. Open this page on your PC</li>
       <li>2. Use your phone to visit <span style="font-style:italic; color:#ffcc00">timzhong.com/drawingboard/pen</span></li>
       <li>3. Enter <span id="boardid"></span> as the board ID and press "Start"</li>
     </ul>
